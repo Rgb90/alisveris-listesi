969155d7a75138a034f71926d44cf213fd9d5335
Rgb90 <143276231+Rgb90@users.noreply.github.com>
2024-01-11 17:32:36 +0300

    first commit

---
+++ src/components/ShoppingList.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+
+const shops = [{
+    id: 1,
+    name: 'Tarım Kredi Koop.'
+  }, {
+    id: 2,
+    name: 'Teknosa'
+  }, {
+    id: 3,
+    name: 'File'
+  }];
+
+  const categories = [{
+    id: 1,
+    name: 'Bakliyat'
+  }, {
+    id: 2,
+    name: 'Teknoloji'
+  }, {
+    id: 3,
+    name: 'Fırın'
+  }];
+
+/*   const [products, setProducts] = useState("") */

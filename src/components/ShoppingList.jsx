14666ca74cf3fce8d2a244cbf6302aa9befaed1d
Rgb90 <143276231+Rgb90@users.noreply.github.com>
2024-01-13 17:00:52 +0300

    add.edit

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,26 +1,80 @@
+import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 
 const shops = [{
-    id: 1,
-    name: 'Tarım Kredi Koop.'
-  }, {
-    id: 2,
-    name: 'Teknosa'
-  }, {
-    id: 3,
-    name: 'File'
-  }];
+  id: 1,
+  name: 'Tarım Kredi Koop.'
+}, {
+  id: 2,
+  name: 'Teknosa'
+}, {
+  id: 3,
+  name: 'BIM'
+}, {
+  id: 4,
+  name: 'File'
+}];
 
-  const categories = [{
-    id: 1,
-    name: 'Bakliyat'
-  }, {
-    id: 2,
-    name: 'Teknoloji'
-  }, {
-    id: 3,
-    name: 'Fırın'
-  }];
+const categories = [{
+  id: 1,
+  name: 'Bakliyat'
+}, {
+  id: 2,
+  name: 'Şarküteri'
+}, {
+  id: 3,
+  name: 'Teknoloji'
+}, {
+  id: 4,
+  name: 'Fırın'
+}];
 
-/*   const [products, setProducts] = useState("") */
+const Wrapper = styled.div`
+display: flex;
+flex-direction: row;
+align-items: end;
+gap: 12px;
+padding: 24px;
+`
+
+export function ShoppingList() {
+const [products, setProducts] = useState([]);
+
+  return (
+    <Form>
+      <Wrapper>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Ürün adı giriniz</Form.Label>
+          <Form.Control 
+          type="text" />
+        </Form.Group>
+
+        <Form.Select 
+        style={{ width: "25%" }}
+        aria-label="Default select example">
+          <option>Market seçiniz</option>
+          {shops.map((shop) => (
+            <option key={shops.id} value={shops.id}>
+              {shop.name}
+            </option>
+          ))}
+        </Form.Select>
+
+        <Form.Select
+        style={{ width: "25%" }}
+        aria-label="Default select example">
+          <option>Kategori seçiniz</option>
+          {categories.map((category) => (
+            <option key={categories.id} value={categories.id}>
+              {category.name}
+            </option>
+          ))}
+        </Form.Select>
+        <Button variant='success'>Ekle</Button>
+      </Wrapper>
+    </Form>
+  );
+}; 
